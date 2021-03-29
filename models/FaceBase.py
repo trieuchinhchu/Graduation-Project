0aa3d9b3e0b0f86be70b8cb3a7e0945bee5266ae
chinh <chutrieuchinh98@gmail.com>
2021-03-30 00:03:15 +0700

    update

--- models/FaceBase.py
+++ models/FaceBase.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import cv2
 
 class FaceBase(models.Model):
     _name = 'da.face.base'
 
     name = fields.Char(string='Name')
+    res_id = fields.Interger(string='res_id')
+
+    cam = cv2.VideoCapture(0)
+    detector = cv2.CascadeClassifier('haarcascade_frontalface_default')
+
+    def insert_data(self, id, name):
+        face_base_obj = self.env['da.face.base']
+        rec = face_base_obj.search([('id', '=', id)])
+        if rec:
+            rec.name = name
+        else:
+            face_base_obj.create({'id': id,
+                                  'name': name})
+    
+    # def open_camera(self):
--- wizards/__init__.py
+++ wizards/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import FaceBase
+from . import insert_data
