8dd83cf0600af40524d33b1e32069f21063a158f
chinh.chutrieu <chinh.chutrieu@vti.com.vn>
2021-06-09 17:02:32 +0700

    preview images

--- facebase/models/hr_employee.py
+++ facebase/models/hr_employee.py
@@ -9,6 +9,17 @@ class HrEmployeeFacebaseInherit(models.Model):
     _inherit = 'hr.employee'
     _description = 'HR Employee Facebase Inherit'
 
-    images = fields.Many2many(string='Images', comodel_name='ir.attachment',
+    images = fields.Many2many(string='Images',
+                              comodel_name='ir.attachment',
                               required=True,
                               relation='hr_employee_facebase_images_rel')
+
+    view_images = fields.Many2many(string='View Images',
+                                   comodel_name='ir.attachment',
+                                   compute='_compute_view_images')
+
+    @api.depends('images')
+    def _compute_view_images(self):
+        for r in self:
+            if r.images:
+                r.view_images = [(6, 0, r.images.ids)]
